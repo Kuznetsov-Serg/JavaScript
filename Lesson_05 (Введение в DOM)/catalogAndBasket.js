50edd0710a43d559d1b35110a9f7dfde42dab542
Kuznetsov-Serg <ksn1974@mail.ru>
2021-06-21 13:39:09 +0300

    HomeWork_05_correct_01

--- Lesson_05 (Введение в DOM)/catalogAndBasket.js
+++ Lesson_05 (Введение в DOM)/catalogAndBasket.js
@@ -15,46 +15,153 @@
 
 'use strict';
 
-const products = [
+const catalog = [
     { id: 1, name: "NoteBook", measureUnit: 'шт', flagArh: false },
     { id: 2, name: "Monitor", measureUnit: 'шт', flagArh: false },
     { id: 3, name: "Mouse", measureUnit: 'шт', flagArh: true },
     { id: 4, name: "KeyBoard", measureUnit: 'шт', flagArh: false }
-]
+];
 
 const price = [
     { id: 1, idProduct: 1, cost: 150, },
     { id: 2, idProduct: 2, cost: 250, },
     { id: 3, idProduct: 3, cost: 350, },
     { id: 4, idProduct: 4, cost: 400, }
-]
+];
 
-let basket = [];
+const basket = [
+    { id: 1, idPrice: 1, count: 3 },
+    { id: 2, idPrice: 2, count: 2 },
+    { id: 3, idPrice: 4, count: 4 }
+];
 
-function initBasket() {
-    basket = [
-        { id: 1, idPrice: 1, count: 3 },
-        { id: 2, idPrice: 2, count: 2 },
-        { id: 3, idPrice: 4, count: 4 }
-    ]
+
+// function getProductById(products, id) {
+//     for (let val of products) {
+//         if (id == val.id) return val;
+//     }
+// }
+
+// Объект "Каталог"
+const catalogObj = {
+    item: [],
+
+    init(catalog) {
+        this.containerElement = document.getElementById('catalog_table');
+        this.item = JSON.parse(JSON.stringify(catalog));        // глубокое клонирование
+        this.render();
+    },
+    render() {
+        for (let row of this.item) {
+            this.containerElement.insertAdjacentHTML('beforeend', `<tr><td>${row.name}
+                    </td><td>${row.measureUnit}</td><td>${row.flagArh ? '√' : ''}</td></tr>`);
+        }
+    },
+    getElementById(id) {
+        for (let val of this.item) {
+            if (val.id == id) return val;
+        }
+    },
 }
 
-function initCellsCatalog() {
-    let containerElement = document.getElementById('catalog_table');
-    let trElem, cell;
 
-    for (let row of products) {
-        // trElem = document.createElement('tr');
-        // containerElement.appendChild(trElem);
+// Объект "Прайс"
+const priceObj = {
+    item: [],
 
-        // appendTd(trElem, row.name);
-        // appendTd(trElem, row.measureUnit);
-        // appendTd(trElem, (row.flagArh ? '√' : ''));
+    init(price, catalogObj) {
+        for (let val of price) {
+            const product = catalogObj.getElementById(val.idProduct);
+            val.productName = product.name;
+            this.item.push(val);
+        }
+    },
+    getElementById(id) {
+        for (let val of this.item) {
+            if (val.id == id) return val;
+        }
+    },
+}
 
-        // Более краткая запись: 
-        containerElement.insertAdjacentHTML('beforeend', `<tr><td>${row.name}
-                </td><td>${row.measureUnit}</td><td>${row.flagArh ? '√' : ''}</td></tr>`);
-    }
+// Объект "Корзина"
+const basketObj = {
+    item: [],
+    priceObj,
+    saveBasket: [],
+    containerElement: null,
+    footerStr: null,
+    cartButton: null,
+
+    init(basket, priceObj) {
+        this.containerElement = document.getElementById('basket_table');
+        this.footerStr = document.getElementById('basket_total');
+        this.cartButton = document.querySelector('.my-btn');
+        this.cartButton.addEventListener('click', this.delProduct.bind(this));  // Кнопка для работы с содержимым Корзины (bind для подстановки объекта)
+
+        this.priceObj = priceObj;
+        for (let val of basket) {
+            const price = this.priceObj.getElementById(val.idPrice)
+            val.productName = price.productName;
+            val.cost = price.cost;
+            this.item.push(val);
+        }
+        this.saveBasket = JSON.parse(JSON.stringify(this.item));    // сохраним на случай переЗаполнения корзины (глубокое клонирование)
+        this.render();
+    },
+    getTotalSum() {
+        return this.item.reduce(function (price, item) {
+            return price + item.cost * item.count;
+        }, 0);
+    },
+    getTotalCount() {
+        return this.item.length;
+    },
+
+    render() {
+        this.delBasketWindow();
+        let trElem, cell, totalCount = 0;
+
+        for (let row of this.item) {
+            totalCount++;
+
+            trElem = document.createElement('tr');
+            this.containerElement.appendChild(trElem);
+
+            appendTd(trElem, totalCount);
+            appendTd(trElem, row.productName);
+            appendTd(trElem, row.count);
+            appendTd(trElem, row.cost);
+            appendTd(trElem, row.count * row.cost);
+        }
+        if (totalCount != 0) {                          // Корзина НЕ пуста
+            this.containerElement.style.display = "block";   // Покажем верх таблицы с заголовком
+            this.footerStr.textContent = `В корзине: ${totalCount} товара(ов) на сумму ${this.getTotalSum()}`;
+        }
+        else {
+            this.containerElement.style.display = "none";    // Уберем верх таблицы с заголовком
+            this.footerStr.textContent = "Корзина пуста";
+        }
+    },
+    // удалим нижнюю запись из корзины
+    delProduct() {
+        if (this.item.length == 0) {       // корзина пуста --> заполним ее 
+            this.item = JSON.parse(JSON.stringify(this.saveBasket));    // вернем первоначальную корзину (глубокое клонирование)
+            this.cartButton.textContent = 'Удалить последнюю позицию из Корзины';
+        }
+        else {
+            this.item.pop();
+            if (this.item.length == 0)
+                this.cartButton.textContent = 'Заполнить корзину';
+        }
+        this.render();
+    },
+    // Удалим из таблицы "Корзина" все, кроме заголовка
+    delBasketWindow() {
+        const row = document.querySelector('#basket_table').querySelectorAll(`tr`);
+        for (let i = 1; i < row.length; i++) {
+            row[i].remove();
+        }
+    },
 }
 
 function appendTd(parent, val) {
@@ -65,75 +172,6 @@ function appendTd(parent, val) {
     parent.insertAdjacentHTML('beforeend', `<td>${val}</td>`);  // более краткий аналог
 }
 
-function initCellsBasket() {
-    let containerElement = document.getElementById('basket_table');
-    let trElem, cell, totalSum = 0, totalCount = 0;
-
-    for (let row of basket) {
-        const priceStr = getPriseById(price, row.idPrice);
-        const productStr = getProductById(products, priceStr.idProduct)
-        totalCount++;
-        totalSum += row.count * priceStr.cost;
-
-        trElem = document.createElement('tr');
-        containerElement.appendChild(trElem);
-
-        appendTd(trElem, totalCount);
-        appendTd(trElem, productStr.name);
-        appendTd(trElem, row.count);
-        appendTd(trElem, priceStr.cost);
-        appendTd(trElem, row.count * priceStr.cost);
-    }
-    let footerStr = document.getElementById('basket_total');
-    if (totalCount != 0) {                          // Корзина НЕ пуста
-        containerElement.style.display = "block";   // Покажем верх таблицы с заголовком
-        footerStr.textContent = `В корзине: ${totalCount} товара(ов) на сумму ${totalSum} рублей`;
-    }
-    else {
-        containerElement.style.display = "none";    // Уберем верх таблицы с заголовком
-        footerStr.textContent = "Корзина пуста";
-    }
-}
-
-function getPriseById(price, id) {
-    for (let val of price) {
-        if (val.id == id) return val;
-    }
-}
-
-function getProductById(product, id) {
-    for (let val of product) {
-        if (val.id == id) return val;
-    }
-}
-
-// Удалим из таблицы "Корзина" все, кроме заголовка
-function delBasket() {
-    const row = document.querySelector('#basket_table').querySelectorAll(`tr`);
-    for (let i = 1; i < row.length; i++) {
-        row[i].remove();
-    }
-}
-
-// удалим нижнюю запись из корзины
-function delProductFromBasket() {
-    delBasket();
-    if (basket.length == 0) {       // корзина пуста --> заполним ее 
-        initBasket();
-        btn.textContent = 'Удалить последнюю позицию из Корзины';
-    }
-    else {
-        basket.pop();
-        if (basket.length == 0) btn.textContent = 'Заполнить корзину';
-    }
-    initCellsBasket();
-}
-
-initBasket();
-initCellsCatalog();
-initCellsBasket();
-const btn = document.querySelector('.my-btn');
-btn.addEventListener('click', delProductFromBasket);
-
-
-
+catalogObj.init(catalog);               // объект "Каталог"
+priceObj.init(price, catalogObj);       // Объект "Прайс"
+basketObj.init(basket, priceObj);       // объект "Корзина"
--- Lesson_05 (Введение в DOM)/chessBoard.js
+++ Lesson_05 (Введение в DOM)/chessBoard.js
@@ -7,41 +7,126 @@
  *      Строки должны нумероваться числами от 1 до 8, столбцы – латинскими буквами A, B, C, D, E, F, G, H.
  */
 
-function initCells() {
-    let containerElement = document.getElementById('chessBoard');
-    let rowCount = 8, colCount = 8;
-    let titleRow = ' 87654321 ';
-    let titleCol = ' abcdefgh ';
-    let borderOut = '1px solid black';
 
-    for (let row = 0; row < rowCount + 2; row++) {
-        const trElem = document.createElement('tr');
-        containerElement.appendChild(trElem);
+/**
+ * @property {HTMLElement} gameContainerEl Контейнер игры.
+ * @property {{name: string, color: string, pos: string}[]} figures Фигуры в игре.
+ * @property {object} figureHtml Содержит информацию о том, как отобразить фигуры на поле.
+ */
+const chess = {
+    containerElement: document.getElementById('chessBoard'),
 
-        for (let col = 0; col < colCount + 2; col++) {
-            const cell = document.createElement('td');
-            if (row == 0 || col == 0 || row == rowCount + 1 || col == colCount + 1) {    // Клетки окантовки (заголовки)
-                if (row == 0 || row == rowCount + 1)
-                    cell.textContent = titleCol[col];
-                else
-                    cell.textContent = titleRow[row];
-                // if (row == 0 || col == colCount + 1)    // косячит внешний бордюр по краю доски...
-                if (row == 0 && col != 0 && col != colCount + 1 || col == colCount + 1 && row != 0)
-                    cell.style.transform = "rotate(180deg)";    // верхняя строка и правая колонка перевернуты для 2-го игрока
-                // сделаем красивый край доски
-                cell.style.border = "none";
-                if (row == 0) cell.style.borderTop = borderOut;
-                if (row == rowCount + 1) cell.style.borderBottom = borderOut;
-                if (col == 0) cell.style.borderLeft = borderOut;
-                if (col == colCount + 1) cell.style.borderRight = borderOut;
+    figures: [
+        { name: 'p', color: 'w', pos: 'a2' },
+        { name: 'p', color: 'w', pos: 'b2' },
+        { name: 'p', color: 'w', pos: 'c2' },
+        { name: 'p', color: 'w', pos: 'd2' },
+        { name: 'p', color: 'w', pos: 'e2' },
+        { name: 'p', color: 'w', pos: 'f2' },
+        { name: 'p', color: 'w', pos: 'g2' },
+        { name: 'p', color: 'w', pos: 'h2' },
+        { name: 'R', color: 'w', pos: 'a1' },
+        { name: 'N', color: 'w', pos: 'b1' },
+        { name: 'B', color: 'w', pos: 'c1' },
+        { name: 'Q', color: 'w', pos: 'd1' },
+        { name: 'K', color: 'w', pos: 'e1' },
+        { name: 'B', color: 'w', pos: 'f1' },
+        { name: 'N', color: 'w', pos: 'g1' },
+        { name: 'R', color: 'w', pos: 'h1' },
+
+        { name: 'p', color: 'b', pos: 'a7' },
+        { name: 'p', color: 'b', pos: 'b7' },
+        { name: 'p', color: 'b', pos: 'c7' },
+        { name: 'p', color: 'b', pos: 'd7' },
+        { name: 'p', color: 'b', pos: 'e7' },
+        { name: 'p', color: 'b', pos: 'f7' },
+        { name: 'p', color: 'b', pos: 'g7' },
+        { name: 'p', color: 'b', pos: 'h7' },
+        { name: 'R', color: 'b', pos: 'a8' },
+        { name: 'N', color: 'b', pos: 'b8' },
+        { name: 'B', color: 'b', pos: 'c8' },
+        { name: 'Q', color: 'b', pos: 'd8' },
+        { name: 'K', color: 'b', pos: 'e8' },
+        { name: 'B', color: 'b', pos: 'f8' },
+        { name: 'N', color: 'b', pos: 'g8' },
+        { name: 'R', color: 'b', pos: 'h8' },
+    ],
+    figureHtml: {
+        pw: '&#9817;',
+        Bw: '&#9815;',
+        Nw: '&#9816;',
+        Rw: '&#9814;',
+        Qw: '&#9813;',
+        Kw: '&#9812;',
+
+        pb: '&#9823;',
+        Bb: '&#9821;',
+        Nb: '&#9822;',
+        Rb: '&#9820;',
+        Qb: '&#9819;',
+        Kb: '&#9818;',
+    },
+
+    renderMap() {
+        let containerElement = document.getElementById('chessBoard');
+        let rowCount = 8, colCount = 8;
+        let titleRow = ' 87654321 ';
+        let titleCol = ' abcdefgh ';
+        let borderOut = '1px solid black';
+
+        for (let row = 0; row < rowCount + 2; row++) {
+            const trElem = document.createElement('tr');
+            containerElement.appendChild(trElem);
+
+            for (let col = 0; col < colCount + 2; col++) {
+                const cell = document.createElement('td');
+
+                // Добавляем каждому полю data аттрибуты о номере колонки и номере строки.
+                cell.dataset.row = titleRow[row].toString();
+                cell.dataset.col = titleCol[col].toString();
+
+                if (row == 0 || col == 0 || row == rowCount + 1 || col == colCount + 1) {    // Клетки окантовки (заголовки)
+                    if (row == 0 || row == rowCount + 1)
+                        cell.textContent = titleCol[col];
+                    else
+                        cell.textContent = titleRow[row];
+                    // if (row == 0 || col == colCount + 1)    // косячит внешний бордюр по краю доски...
+                    if (row == 0 && col != 0 && col != colCount + 1 || col == colCount + 1 && row != 0)
+                        cell.style.transform = "rotate(180deg)";    // верхняя строка и правая колонка перевернуты для 2-го игрока
+                    // сделаем красивый край доски
+                    cell.style.border = "none";
+                    if (row == 0) cell.style.borderTop = borderOut;
+                    if (row == rowCount + 1) cell.style.borderBottom = borderOut;
+                    if (col == 0) cell.style.borderLeft = borderOut;
+                    if (col == colCount + 1) cell.style.borderRight = borderOut;
+                }
+                else {
+                    if (row % 2 != col % 2)
+                        cell.style.backgroundColor = "gray";
+                }
+                trElem.appendChild(cell);
             }
-            else {
-                if (row % 2 != col % 2)
-                    cell.style.backgroundColor = "black";
-            }
-            trElem.appendChild(cell);
         }
+    },
+    /**
+     * Отображает фигуры на поле.
+     */
+    renderFigures() {
+        // Перебираем все фигуры, которые есть в массиве.
+        for (const figure of this.figures) {
+            // Получаем колонку и строку, где стоит фигура.
+            const col = figure.pos.charAt(0);
+            const row = figure.pos.charAt(1);
+            // Находим нужную ячейку, ставим ей innerHTML взятый из объекта this.figureHtml,
+            // ключ - это два символа, имя фигуры и цвет, в итоге получим символ фигуры.
+            document.querySelector(`[data-col='${col}'][data-row='${row}']`).innerHTML =
+                this.figureHtml[figure.name + figure.color];
+        }
+
     }
 }
 
-initCells();
+// Запускаем метод отображения карты.
+chess.renderMap();
+// Запускаем метод отображения фигур.
+chess.renderFigures();
